d6e64d9f5a39ff0f3ef294c1757da2a025d38fc8
Jesse Evans <jevans40@uwyo.edu>
2019-03-30 15:28:21 -0600

    Cleaning Up the Repository

--- Omega-Phi/Window.h
+++ Omega-Phi/Window.h
@@ -81,6 +81,8 @@ namespace OP {
 		GLFWwindow* getWindow();
 
 
+		void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
 		static void error_callback(int error, const char* description)
 		{
 			fprintf(stderr, "Error: %s\n", description);
